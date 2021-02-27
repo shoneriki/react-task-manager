42b1659e0921ddbabf71758552e81fdb28173e87
shoneriki <ikirenohs@gmail.com>
2021-02-27 17:24:25 +0900

    index.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-
+import React from 'react'
+import Header from './components/Header'
 
 function App() {
-  const name = 'Sho'
-  // 2/27/2021-4:50..22.58
-  // variable defined above return, and put into h2 dynamically in curly brackets
-  const x = false
-
-  return (
-    // this is JSX, not html
-    <div className='container'>
-      <h1>Hello From React</h1>
-      {/* can only have one parent element, h2 shouldn't go outside app */}
-      <h2>Hello, {name}</h2>
+  return(
+    <div className = 'container'>
+      <Header />
     </div>
-  );
+  )
 }
-
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,32 @@
 // 22.58 components folder for all components, create header.js file
+// rafce tab for shortcut for boilerplate from extension es7
+// take out the import react
+import PropTypes from 'prop-types'
+
+
+
+
+const Header = ({ title }) => {
+  return (
+    <header className = 'header'>
+      <h1>{title}</h1>
+      <button className='btn'>Add</button>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// }
+
+export default Header
